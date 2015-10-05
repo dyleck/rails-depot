a5b3b00d4f43efb959ceda92a320bf661dc75ecc
rails <rails@node1.dylux.lab>
2015-10-05 02:27:01 +0200

    Chapter 6 completed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '897a219e570babc842065382a70f3fae811555c73d5ed2c0ea889cc5701505d5d6240d52a60ba0097249da34ea5fd8d86220cb47f25d008aa50969a91c147c09'
