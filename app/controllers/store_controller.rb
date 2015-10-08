ab9a31b484c6dec57436a660f0326158be6cd948
Marcin Dylewski <bounceerr@gmail.com>
2015-10-08 13:14:56 +0200

    counter added and displayed at catalog page only if 5 times accessed. Counter resets each time product is added.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,9 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    @counter = session[:counter] += 1
   end
 end
