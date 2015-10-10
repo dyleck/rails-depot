cf76369202a3765870af5960759aedb383971742
Marcin Dylewski <bounceerr@gmail.com>
2015-10-10 17:48:40 +0200

    Added remove from cart button (descrease quantity if > 1). Functional test updated.

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,9 +57,16 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    if @line_item.quantity > 1
+      @line_item.quantity -= 1
+      @line_item.save!
+      notice = "Quantity descreased."
+    else
+      @line_item.destroy
+      notice = "Product has been removed."
+    end
     respond_to do |format|
-      format.html { redirect_to line_items_url }
+      format.html { redirect_to cart_url(session[:cart_id]), notice: notice }
       format.json { head :no_content }
     end
   end
