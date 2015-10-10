0130ef1d399b17fccbb24af361ea82dfe00694a1
Marcin Dylewski <bounceerr@gmail.com>
2015-10-10 15:45:59 +0200

    ex2 - test for different products addded to cart

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -6,9 +6,12 @@ class CartTest < ActiveSupport::TestCase
     cart.add_product(products(:ruby).id)
     cart.save!
     assert_equal 1, cart.line_items.count, "should create single line_item"
+    cart.add_product(products(:python).id)
+    cart.save!
+    assert_equal 2, cart.line_items.count, "should create two line_items"
     line_item = cart.add_product(products(:ruby).id)
     cart.save!
-    assert_equal 1, cart.line_items.count, "should create single line item"
+    assert_equal 2, cart.line_items.count, "should create single line item"
     assert_equal 2, line_item.quantity, "should have quantity of 2"
   end
 end
