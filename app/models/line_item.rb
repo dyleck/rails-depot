f61a1aee57d7132af5f1a1503f40f9abef68ba31
Marcin Dylewski <bounceerr@gmail.com>
2015-10-21 21:17:59 +0200

    ex2 Convert value to euros and display it when locale is es

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -4,6 +4,10 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
 
   def total_price
-    product.price * quantity
+    if I18n.locale.to_s.downcase == 'es'
+      product.price * quantity * 2
+    else
+      product.price * quantity
+    end
   end
 end
